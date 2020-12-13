a6195803829f6e7da09ba65f84703019cca4236f
huynhthaianhhd <huynhthaianhhd@gmail.com>
2020-12-13 22:55:12 +0700

    complete handle socket for show list user

--- config/socket.js
+++ config/socket.js
@@ -7,20 +7,30 @@ module.exports = {
     });
     let currentUser = [];
     io.on("connection", (client) => {
+      client.on("disconnect", function () {
+        console.log("Disconnected: ", client.id);
+        currentUser = currentUser.filter((e) => e.clientId !== client.id);
+        io.sockets.emit("onlineUser", currentUser);
+      });
+
+      console.log("Connected: ", client.id);
       client.on("client-login", (res) => {
         const { user } = res;
-        currentUser.push(user);
+        const userLogin = { ...user, clientId: client.id };
+        currentUser.push(userLogin);
+        io.sockets.emit("onlineUser", currentUser);
+      });
+      client.on("client-get-list", (res) => {
         io.sockets.emit("onlineUser", currentUser);
       });
       client.on("client-logout", (res) => {
         const { user } = res;
-        currentUser = currentUser.filter((e) => e.email !== user.email);
+        const index = currentUser.findIndex(
+          (item) => user.email === item.email
+        );
+        currentUser.splice(index, 1);
         io.sockets.emit("onlineUser", currentUser);
       });
-    });
-
-    io.on("disconnect", (client) => {
-      console.log("id", client.id);
     });
   },
 };
