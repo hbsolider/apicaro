fe68561eb63306ce2103f95339511f181d2db7ed
huynhthaianhhd <55442707+huynhthaianhhd@users.noreply.github.com>
2020-12-16 02:55:44 +0700

    handle create room & join room & show list room (#3)

--- src/sockets/index.js
+++ src/sockets/index.js
@@ -1,11 +1,13 @@
 import getOnlineList from './getOnlineList';
 import chatWorld from './chat/chatWorld';
 import chatRoom from './chat/chatRoom';
+import getRoomList from './getRoomList';
 import game from './game';
 const initSockets = (io) => {
   getOnlineList(io);
   chatWorld(io);
   chatRoom(io);
+  getRoomList(io);
   game(io);
 };
 
