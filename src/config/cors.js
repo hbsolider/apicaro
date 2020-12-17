7d3142ca15b23325c07b3642641097c15d266107
HoaiBao <rorolo2011@gmail.com>
2020-12-17 12:32:58 +0700

    update

--- src/config/cors.js
+++ src/config/cors.js
@@ -4,7 +4,11 @@ let origin;
 if (process.env.NODE_ENV === 'development') {
   origin = 'http://localhost:3000';
 } else {
-  origin = ['http://localhost:3000','https://socketserve.io/'];
+  origin = [
+    'http://localhost:3000',
+    'https://socketserve.io/',
+    'https://caro-game-anhem1nha.herokuapp.com/',
+  ];
 }
 module.exports = {
   config: (app) => {
