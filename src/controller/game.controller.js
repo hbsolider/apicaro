077f7d19008869e6cfcbeede914f99ec0d97b5fa
huynhthaianhhd <huynhthaianhhd@gmail.com>
2021-01-10 07:03:14 +0700

    api get list game of user

--- src/controller/game.controller.js
+++ src/controller/game.controller.js
@@ -1,10 +1,17 @@
 import gameService from 'service/game.service';
+
 export const getListGame = async (req, res) => {
-//   let page = req.query.page || 1;
-//   let limit = req.query.limit || 11;
+  //   let page = req.query.page || 1;
+  //   let limit = req.query.limit || 11;
   const list = await gameService.getAll();
   if (list) {
     return res.json({ message: 'get list game success', data: list });
   }
   return res.json({ message: 'get list game failed', data: null });
+};
+
+export const getListGameByUser = async (req, res) => {
+  const { user } = req;
+  const games = await gameService.listGamesByUser(user.id);
+  return res.json({ message: 'Success', data: games });
 };
--- src/router/game.router.js
+++ src/router/game.router.js
@@ -1,7 +1,10 @@
 const router = require('express').Router();
 const errorHandle = require('../utils/errorHandle');
+import { auth } from 'middleware/auth';
 import { getListGame } from 'controller/game.controller';
+import { getListGameByUser } from 'controller/game.controller';
 
 router.get('/', errorHandle(getListGame));
+router.get('/user', auth(), getListGameByUser);
 
 module.exports = router;
